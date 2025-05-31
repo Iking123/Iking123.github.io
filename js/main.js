c26d459d5150cd718eb04b6d4e45c796d9e97d00
Iking <754261629@qq.com>
2025-06-01 01:27:14 +0800

    Site updated: 2025-06-01 01:27:14

--- js/main.js
+++ js/main.js
@@ -41,35 +41,3 @@ const app = Vue.createApp({
     },
 });
 app.mount("#layout");
-
-
-// 来自Createsequence的自动截取摘要~
-window.onload = function() {
-  return getAbstract();
-}
-
-/**
- * 截取摘要
- */
-function getAbstract() {
-    let arts = document.getElementsByTagName('article');
-    if (arts.length <= 1) {
-        console.log("不在主页！");
-        return ;
-    }
-    for (let i = 0; i < arts.length; i++) {
-        let dom = arts[i];
-        // process post body
-        let button = dom.getElementsByClassName("post-button")[0];
-        let body = dom.getElementsByClassName("post-body")[0];
-        body.removeChild(button);
-
-        // resolve abstract content
-        let content = body.textContent;
-        content = content.substring(0, 250) + "......";
-        body.innerHTML = content;
-
-        // append post button
-        body.appendChild(button);
-    }
-}
