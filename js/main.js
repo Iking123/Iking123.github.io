cca2febab61cc5778789a2eeba51ccbc1be7d53e
Iking <754261629@qq.com>
2025-06-01 01:14:31 +0800

    Site updated: 2025-06-01 01:14:30

--- js/main.js
+++ js/main.js
@@ -41,3 +41,29 @@ const app = Vue.createApp({
     },
 });
 app.mount("#layout");
+
+// 截取摘要
+getAbstract();
+
+/**
+ * 截取摘要
+ */
+function getAbstract() {
+    let posts = document.getElementById('posts');
+
+    if (document.getElementsByTagName('article').length <= 1) {
+        console.log("不在主页！");
+        return ;
+    }
+
+    let arts = posts.getElementsByClassName("post-body");
+    for (let i = 0; i < arts.length; i++) {
+        let dom = arts[i];
+        let content = dom.innerText
+            .substring(0, 250)  + "......";
+
+        let readAll = dom.getElementsByClassName("post-button")[0];
+        dom.innerHTML = content;
+        dom.appendChild(readAll);
+    }
+}
